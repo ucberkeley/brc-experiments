641403756acaa81c09fab96965c6b71bd9cf9103
Aaron Culich <aculich@gmail.com, aculich@berkeley.edu>
2014-10-16 16:40:02 -0700

    remove string.punctuation from random string generator, closes Issue #3
    Since the passwords are intended to be temporary for first-time login
    only, with the user required to change their password I've just remove
    string.punctuation and will rely on string.letters + string.digits to be
    sufficient for now.

--- aws-iam/provision.py
+++ aws-iam/provision.py
@@ -7,7 +7,7 @@ import csv
 import string
 import random
 
-def random_string(size=10, chars=string.letters + string.digits + string.punctuation):
+def random_string(size=10, chars=string.letters + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def create_iam_users(target, category):
